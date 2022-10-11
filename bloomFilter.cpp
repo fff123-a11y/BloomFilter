9dd00d42275704e81ff6b39075ee65062d92ac4b
hciugefvigr736 <102908697+hciugefvigr736@users.noreply.github.com>
2022-10-11 22:00:04 +0800

    Create bloomFilter.cpp

---
+++ bloomFilter.cpp
@@ -0,0 +1,19 @@
+#include "bloomFilter.h"
+
+void InitBloomFilter(BF* bf, PHF hashFunc[FUNCNUM], size_t size)
+{
+}
+
+int InsertBF(BF* bf, DataType key)
+{
+    return 0;
+}
+
+int IsInBloomFilter(BF* bf, DataType key)
+{
+    return 0;
+}
+
+void DestroyBloomFilter(BF* bf)
+{
+}
